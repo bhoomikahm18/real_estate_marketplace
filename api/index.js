bcebb181e2045ecb3f772c98be7361765d54c54d
Bhoomika <bhoomikahm18@gmail.com>
2024-06-05 20:30:44 +0530

    create a auth route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
+import authRouter from "./routes/authRoute.js";
 dotenv.config();
 
 mongoose
@@ -11,6 +12,7 @@ mongoose
 
 const app = express();
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
